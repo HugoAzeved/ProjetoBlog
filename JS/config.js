d382dac711c045cd17957b2b8892391bec554f7f
Hugo <hugoazevedo649@gmail.com>
2025-01-22 21:46:44 -0300

    novo

---
+++ JS/config.js
@@ -0,0 +1,25 @@
+/**
+ * configuração do site;
+ */
+
+const site = {
+    /**
+      * Nome do site usado na tag <title>...</title> e nas interações dinâmicas
+      **/
+    nome: "Chrono Mirror",
+
+    /**
+     * Logotipo do site, usado na tag <header>...</title>
+     */
+    logo: "img/logo.png",
+
+    /**
+     * Controla a ação ao clicar no link do usuário logado no menu principal
+     * Se `true`, exibe o perfil do usuário → perfil.html
+     * Se `false`, faz logout direto
+     **/
+    verPerfil: false,
+
+    proprietario: "Hugo Az"
+    
+}
---
+++ JS/global.js
@@ -0,0 +1,16 @@
+
+
+
+
+
+// Arrow function
+window.onload = () => {
+
+    document.title = site.nome
+
+    // Carrega o template HTML em div#wrap
+    _('#wrap').innerHTML = template();
+
+    _('#conteudo').innerHTML = ''
+
+}
